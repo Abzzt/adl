6a5078fdc9e143ec4f078322a074e356746af9df
kayweeee <tankaywee09@gmail.com>
2024-04-17 04:40:26 +0800

    remove: accuracy in eval

--- helper.py
+++ helper.py
@@ -17,21 +17,15 @@ def train(model, train_loader, optimizer, criterion, device):
 def evaluate(model, valid_loader, criterion, device):
     model.eval()
     valid_loss = 0
-    correct = 0
-    total = 0
+
     with torch.no_grad():
         for images, labels in valid_loader:
             images, labels = images.to(device), labels.to(device)
             outputs = model(images)
 
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
             loss = criterion(outputs, labels)
             valid_loss += loss.item()
     
     valid_loss /= len(valid_loader)
-    valid_accuracy = 100 * correct / total
 
-    return valid_loss, valid_accuracy
+    return valid_loss
